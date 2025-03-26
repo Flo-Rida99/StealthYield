7071452f61a822fdaabbf810ea0c0fc8814373dd
Flo-Rida99 <florine.elskamp@gmail.com>
2025-03-26 14:47:26 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: 'export',
+    basePath: '/StealthYield',
+    images: {
+      unoptimized: true,
+    },
+  };
+
+  module.exports = nextConfig;
